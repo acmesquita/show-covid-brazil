effe5951d5273a0813875ae7992474462412dedd
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-24 18:14:14 -0300

    Configurando projeto para o styled-componets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import api from './service';
-import './App.css'
+import { Container } from './styles/app.style.js'
 
 function App() {
   const [data, setData] = useState(JSON.parse(localStorage.getItem('result_covid')) || [])
@@ -30,7 +30,7 @@ function App() {
   },[data, country])
 
   return (
-    <div className='container'>
+    <Container>
       <h1>{country} - COVID19</h1>
       {data.length > 0 &&
         <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} activeDot={{ r: 8 }}>
@@ -43,7 +43,7 @@ function App() {
           <Legend />
         </LineChart>
       }
-    </div>
+    </Container>
   );
 }
 
---
+++ src/styles/app.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: whitesmoke;
+  padding: 20px;
+  border-radius: 3px;
+
+  color: teal;
+  h1{
+    margin-bottom: 10px;
+    text-transform: uppercase;
+  }
+  
+`;
