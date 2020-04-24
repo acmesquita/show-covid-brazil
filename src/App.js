e3d2c4e7406dede9b295392877ce9930e98f5a00
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-24 14:59:33 -0300

    Configurando gráfico

--- src/App.js
+++ src/App.js
@@ -33,10 +33,10 @@ function App() {
     <div className='container'>
       <h1>{country} - COVID19</h1>
       {data.length > 0 &&
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-          <Line type="monotone" dataKey="confirmed" stroke="#8884d8" />
-          <Line type="monotone" dataKey="death" stroke="#ab0c0c" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} activeDot={{ r: 8 }}>
+          <Line type="monotone" dataKey="confirmed" stroke="#8884d8" strokeWidth={2} />
+          <Line type="monotone" dataKey="death" stroke="#ab0c0c" strokeWidth={2}/>
+          <CartesianGrid stroke="#ccc" strokeDasharray="2 2" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
